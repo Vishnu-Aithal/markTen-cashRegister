49d2a29f6109eeb93775c194af291b020929eb12
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2021-09-24 14:20:01 +0530

    bugfix - bill was accepting empty input

--- index.js
+++ index.js
@@ -7,73 +7,79 @@ var messageText = document.querySelector(".message")
 var notes = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
 
 function showCashInput() {
-    billInput.setAttribute("disabled", "");
-    nextBtn.style.display = "none"
-    document.querySelectorAll(".after-bill").forEach((element) => element.style.display = "block");
-    cashInput.focus();
-}
-
-function amountValid(bill, cash) {
-    return (cash > bill);
-}
-
-function calculateNotes(change) {
-    var requiredNotes=[]
-    var remainingChange = change
-    notes.map((note)=>{
-        requiredNotes.push(Math.floor(remainingChange/note));
-        remainingChange = remainingChange%note;
-    })
-    return requiredNotes;
-}
-
-function showTable(notesToDisplay){
-    document.querySelector("table").style.display = "inline-table";
-    
-    document.querySelectorAll(".notes").forEach((element,index)=>{
-        element.innerText = notesToDisplay[index];
-    })
-}
-
-function clickHandler() {
-    billAmount = parseInt(billInput.value);
-    cashAmount = parseInt(cashInput.value);
-    if (amountValid(billAmount, cashAmount)) {
-        messageText.style.display="block";
-        messageText.innerText = "Return Change";
-        cashInput.setAttribute("disabled","");
-        checkBtn.style.display="none";
-        var changeAmount = cashAmount - billAmount
-        var requiredNotes = calculateNotes(changeAmount);
-        showTable(requiredNotes)
-        resetBtn.style.display = "block"
-    } else if (billAmount === cashAmount) {
-        messageText.style.display="block";
-        messageText.innerText = "The cash given is equal to the bill amount, No change required"
+    if (billInput.value != "") {
+        messageText.style.display = "none"
+        billInput.setAttribute("disabled", "");
+        nextBtn.style.display = "none"
+        document.querySelectorAll(".after-bill").forEach((element) => element.style.display = "block");
+        cashInput.focus();
     } else {
-        messageText.style.display="block";
-        messageText.innerText = "The cash given is less than the Bill amount, Enter the Correct Amount Again"
+        messageText.style.display = "block"
+        messageText.innerText = "Bill Amount cannot be empty"
+    }
+}
+
+    function amountValid(bill, cash) {
+        return (cash > bill);
     }
 
-}
+    function calculateNotes(change) {
+        var requiredNotes = []
+        var remainingChange = change
+        notes.map((note) => {
+            requiredNotes.push(Math.floor(remainingChange / note));
+            remainingChange = remainingChange % note;
+        })
+        return requiredNotes;
+    }
 
-function resetRegister() {
-    billInput.value = "";
-    cashInput.value = "";
-    billInput.removeAttribute("disabled");
-    cashInput.removeAttribute("disabled");
-    nextBtn.style.display = "block";
-    checkBtn.style.display = "none";
-    resetBtn.style.display = "none";
-    messageText.style.display = "none"
-    document.querySelector("table").style.display = "none"
-    document.querySelectorAll(".after-bill").forEach((element) => element.style.display = "none");
+    function showTable(notesToDisplay) {
+        document.querySelector("table").style.display = "inline-table";
+
+        document.querySelectorAll(".notes").forEach((element, index) => {
+            element.innerText = notesToDisplay[index];
+        })
+    }
+
+    function clickHandler() {
+        billAmount = parseInt(billInput.value);
+        cashAmount = parseInt(cashInput.value);
+        if (amountValid(billAmount, cashAmount)) {
+            messageText.style.display = "block";
+            messageText.innerText = "Return Change";
+            cashInput.setAttribute("disabled", "");
+            checkBtn.style.display = "none";
+            var changeAmount = cashAmount - billAmount
+            var requiredNotes = calculateNotes(changeAmount);
+            showTable(requiredNotes)
+            resetBtn.style.display = "block"
+        } else if (billAmount === cashAmount) {
+            messageText.style.display = "block";
+            messageText.innerText = "The cash given is equal to the bill amount, No change required"
+        } else {
+            messageText.style.display = "block";
+            messageText.innerText = "The cash given is less than the Bill amount, Enter the Correct Amount Again"
+        }
+
+    }
+
+    function resetRegister() {
+        billInput.value = "";
+        cashInput.value = "";
+        billInput.removeAttribute("disabled");
+        cashInput.removeAttribute("disabled");
+        nextBtn.style.display = "block";
+        checkBtn.style.display = "none";
+        resetBtn.style.display = "none";
+        messageText.style.display = "none"
+        document.querySelector("table").style.display = "none"
+        document.querySelectorAll(".after-bill").forEach((element) => element.style.display = "none");
+        billInput.focus();
+
+    }
+
+
     billInput.focus();
-
-}
-
-
-billInput.focus();
-nextBtn.addEventListener("click", showCashInput);
-checkBtn.addEventListener("click", clickHandler);
-resetBtn.addEventListener("click", resetRegister);
+    nextBtn.addEventListener("click", showCashInput);
+    checkBtn.addEventListener("click", clickHandler);
+    resetBtn.addEventListener("click", resetRegister);
