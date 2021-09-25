fe2cf1fc4642e7a60e43f9b819c384b28aabbf79
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2021-09-25 13:54:01 +0530

    coderefactor

--- index.js
+++ index.js
@@ -6,101 +6,101 @@ var resetBtn = document.querySelector("#reset-btn");
 var messageText = document.querySelector(".message")
 var notes = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
 
-billInput.oninput = (event)=>{
-    if ((event.target.validity.valid) & (event.target.value!="")) {
+billInput.oninput = (event) => {
+    if ((event.target.validity.valid) & (event.target.value != "")) {
         nextBtn.removeAttribute("disabled");
         checkBtn.removeAttribute("disabled");
-        
-    } else{
+
+    } else {
         event.target.value = "";
-        nextBtn.setAttribute("disabled","");
+        nextBtn.setAttribute("disabled", "");
         checkBtn.setAttribute("disabled", "");
     }
 
+}
+
+cashInput.oninput = (event) => {
+    if ((event.target.validity.valid) & (event.target.value != "")) {
+        checkBtn.removeAttribute("disabled");
+
+    } else {
+        event.target.value = "";
+        checkBtn.setAttribute("disabled", "");
     }
-
-    cashInput.oninput = (event)=>{
-        if ((event.target.validity.valid) & (event.target.value!="")) {
-            checkBtn.removeAttribute("disabled");
-            
-        } else{
-            event.target.value = "";
-            checkBtn.setAttribute("disabled", "");
-        }
-    
-        }    
-
-function showCashInput() {
-
-        messageText.style.display = "none";
-        nextBtn.style.display = "none";
-        document.querySelectorAll(".after-bill").forEach((element) => element.style.display = "block");
-        cashInput.focus();
 
 }
 
-    function amountValid(bill, cash) {
-        return (cash > bill);
-    }
+function showCashInput() {
 
-    function calculateNotes(change) {
-        var requiredNotes = []
-        var remainingChange = change
-        notes.map((note) => {
-            requiredNotes.push(Math.floor(remainingChange / note));
-            remainingChange = remainingChange % note;
-        })
-        return requiredNotes;
-    }
+    messageText.style.display = "none";
+    nextBtn.style.display = "none";
+    document.querySelectorAll(".after-bill").forEach((element) => element.style.display = "block");
+    cashInput.focus();
 
-    function showTable(notesToDisplay) {
-        document.querySelector("table").style.display = "inline-table";
+}
 
-        document.querySelectorAll(".notes").forEach((element, index) => {
-            element.innerText = notesToDisplay[index];
-        })
-    }
+function amountValid(bill, cash) {
+    return (cash > bill);
+}
 
-    function clickHandler() {
-        billAmount = parseInt(billInput.value);
-        cashAmount = parseInt(cashInput.value);
-        if (amountValid(billAmount, cashAmount)) {
-            messageText.style.display = "block";
-            messageText.innerText = "Return Change";
-            cashInput.setAttribute("disabled", "");
-            billInput.setAttribute("disabled","");
-            checkBtn.style.display = "none";
-            var changeAmount = cashAmount - billAmount
-            var requiredNotes = calculateNotes(changeAmount);
-            showTable(requiredNotes)
-            resetBtn.style.display = "block"
-        } else if (billAmount === cashAmount) {
-            messageText.style.display = "block";
-            messageText.innerText = "The cash given is equal to the bill amount, No change required"
-        } else {
-            messageText.style.display = "block";
-            messageText.innerText = "The cash given is less than the Bill amount, Enter the Correct Amount Again"
-        }
+function calculateNotes(change) {
+    var requiredNotes = []
+    var remainingChange = change
+    notes.map((note) => {
+        requiredNotes.push(Math.floor(remainingChange / note));
+        remainingChange = remainingChange % note;
+    })
+    return requiredNotes;
+}
 
-    }
+function showTable(notesToDisplay) {
+    document.querySelector("table").style.display = "inline-table";
 
-    function resetRegister() {
-        billInput.value = "";
-        cashInput.value = "";
-        billInput.removeAttribute("disabled");
-        cashInput.removeAttribute("disabled");
-        nextBtn.style.display = "block";
+    document.querySelectorAll(".notes").forEach((element, index) => {
+        element.innerText = notesToDisplay[index];
+    })
+}
+
+function clickHandler() {
+    billAmount = parseInt(billInput.value);
+    cashAmount = parseInt(cashInput.value);
+    if (amountValid(billAmount, cashAmount)) {
+        messageText.style.display = "block";
+        messageText.innerText = "Return Change";
+        cashInput.setAttribute("disabled", "");
+        billInput.setAttribute("disabled", "");
         checkBtn.style.display = "none";
-        resetBtn.style.display = "none";
-        messageText.style.display = "none"
-        document.querySelector("table").style.display = "none"
-        document.querySelectorAll(".after-bill").forEach((element) => element.style.display = "none");
-        billInput.focus();
-
+        var changeAmount = cashAmount - billAmount
+        var requiredNotes = calculateNotes(changeAmount);
+        showTable(requiredNotes)
+        resetBtn.style.display = "block"
+    } else if (billAmount === cashAmount) {
+        messageText.style.display = "block";
+        messageText.innerText = "The cash given is equal to the bill amount, No change required"
+    } else {
+        messageText.style.display = "block";
+        messageText.innerText = "The cash given is less than the Bill amount, Enter the Correct Amount Again"
     }
 
+}
 
+function resetRegister() {
+    billInput.value = "";
+    cashInput.value = "";
+    billInput.removeAttribute("disabled");
+    cashInput.removeAttribute("disabled");
+    nextBtn.style.display = "block";
+    checkBtn.style.display = "none";
+    resetBtn.style.display = "none";
+    messageText.style.display = "none"
+    document.querySelector("table").style.display = "none"
+    document.querySelectorAll(".after-bill").forEach((element) => element.style.display = "none");
     billInput.focus();
-    nextBtn.addEventListener("click", showCashInput);
-    checkBtn.addEventListener("click", clickHandler);
-    resetBtn.addEventListener("click", resetRegister);
+
+}
+
+
+billInput.focus();
+nextBtn.addEventListener("click", showCashInput);
+checkBtn.addEventListener("click", clickHandler);
+resetBtn.addEventListener("click", resetRegister);
